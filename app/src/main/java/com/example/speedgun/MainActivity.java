a91a470ff8f5768810d6e6e6edf630032155a4b8
Eli Meril <elimeril.z0.eli@gmail.com>
2020-05-06 20:04:55 +0300

    yes

--- app/src/main/java/com/example/speedgun/MainActivity.java
+++ app/src/main/java/com/example/speedgun/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int x = 5;
+        x = 5 + 2; // עברית אפס
     }
 }
